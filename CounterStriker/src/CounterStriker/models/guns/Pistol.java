b786811c7274c1ac13007ebefe56cbd5214000a5
Krasimir-Bozhilov <72642058+Krasimir-Bozhilov@users.noreply.github.com>
2020-12-07 21:05:59 +0200

    Add files via upload

---
+++ CounterStriker/src/CounterStriker/models/guns/Pistol.java
@@ -0,0 +1,17 @@
+package CounterStriker.models.guns;
+
+public class Pistol extends GunImpl {
+    private static final int BULLETS_AVAILABLE=1;
+    public Pistol(String name, int bulletsCount) {
+        super(name, bulletsCount);
+    }
+
+    @Override
+    public int fire() {
+        if (super.getBulletsCount()<BULLETS_AVAILABLE){
+            return 0;
+        }
+        super.decreaceBullets(BULLETS_AVAILABLE);
+        return BULLETS_AVAILABLE;
+    }
+}
